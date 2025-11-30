45c93862e3751e0918aef6a14c1c6ead2fc555fb
lvadegre <lvadegre>
2025-11-30 01:23:10 +0100

    clean repo for futur archivage

--- PROJET/src/7/master_worker.h
+++
@@ -1,9 +0,0 @@
-#ifndef MASTER_WORKER_H
-#define MASTER_WORKER_H
-
-// On peut mettre ici des éléments propres au couple master/worker :
-//    - des constantes pour rendre plus lisible les comunications
-//    - des fonctions communes (écriture dans un tube, ...)
-
-
-#endif
